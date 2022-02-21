b0270257c3435085d9cde5e6eb9fe587e4ff3fab
mono-tmt <tomita@monosus.co.jp>
2022-02-21 15:26:49 +0900

    :tada: setup project

---
+++ src/server/index.ts
@@ -0,0 +1,30 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import {Server} from 'colyseus';
+import { monitor} from "@colyseus/monitor";
+import { WebSocketTransport } from "@colyseus/ws-transport"
+
+import { CustomRoom } from './rooms/room'
+
+const port = Number(process.env.PORT || 2567);
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+const server = http.createServer(app);
+
+// @see https://docs.colyseus.io/colyseus/server/transport/
+const gameServer = new Server({
+    transport: new WebSocketTransport({
+        server
+    })
+});
+
+gameServer.define('room',CustomRoom)
+
+app.use("/colyseus", monitor());
+
+gameServer.listen(port);
+console.log(`Hello World localhost::${ port }`)
---
+++ src/server/rooms/schema/RoomState.ts
@@ -0,0 +1,9 @@
+import { Schema, type } from "@colyseus/schema";
+
+export class RoomState extends Schema {
+
+    @type("string")
+
+    mySynchronizedProperty: string = 'Hello world'
+
+}
